dc64d61187d561ce5ab4bcd8a01150461e60ace3
SK-WebDeveloper <vermasanjaykumar97@gmail.com>
2022-07-13 22:10:22 +0530

    Navbar using Bootstrap..1

---
+++ textutils/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar title="TextUtils" about="About" home="Home"/>
+    </div>
+  );
+}
+
+export default App;
